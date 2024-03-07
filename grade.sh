84feec83442647b8f408bbd35a96169ec7c7fe77
Tianchang Wang <20021119wtc@gmail.com>
2024-03-07 08:56:44 -0800

    updated

--- grade.sh
+++ grade.sh
@@ -12,5 +12,30 @@ echo 'Finished cloning'
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
+cp student-submission/Solution.java grading-area/
+cp student-submission/SolutionTest.java grading-area/
+
+cd grading-area
+
+javac -cp "$CPATH" *.java
+if [ $? -ne 0 ]; then
+    echo "Compilation failed. Please check your code for errors."
+    exit 1
+else
+    echo "Compilation successful."
+fi
+
+
+
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+java -cp "$CPATH" org.junit.runner.JUnitCore SolutionTest
+if [ $? -ne 0 ]; then
+    echo "Some tests failed. Please review the test results above for details."
+else
+    echo "All tests passed successfully."
+fi
+
+
+cd ..
